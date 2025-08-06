ac2803edcdb8ccf7bbd31a12c5b1f8ad79558f10
grygoriev <vovagrigoriev@gmail.com>
2025-08-06 19:15:25 +0300

    feat: adds tests for notifier
    task4

--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,75 @@
-import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import { act, render, screen, cleanup, waitFor } from '@testing-library/react';
+import uE from '@testing-library/user-event';
+import { NewTaskBar } from 'src/modules/NewTaskBar';
+import { TaskList } from 'src/modules/TaskList';
+import { NotifierContainer } from 'src/modules/NotifierContainer';
+import { JestStoreProvider } from '../utils/JestStoreProvider';
 
-describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
+beforeAll(() => jest.useFakeTimers());
+afterEach(() => {
+	cleanup();
+	localStorage.clear();
+	jest.clearAllTimers();
+});
+
+const userEvent = uE.setup({ advanceTimers: jest.advanceTimersByTime });
+
+const addTaskUI = async (txt: string) => {
+	await userEvent.type(screen.getByRole('textbox'), txt);
+	await userEvent.click(screen.getByRole('button', { name: /добавить/i }));
+};
+
+const toggleTaskUI = async (header: string) => {
+	await userEvent.click(screen.getByText(header));
+};
+
+describe('Оповещение при выполнении задачи', () => {
+	it('появляется и содержит заголовок задачи', async () => {
+		render(
+			<>
+				<NewTaskBar />
+				<TaskList />
+				<NotifierContainer />
+			</>,
+			{ wrapper: JestStoreProvider },
+		);
+
+		await addTaskUI('купить хлеб');
+		await toggleTaskUI('купить хлеб');
+
+		const notifier = await screen.findByText(/Задача "купить хлеб" завершена/i);
+		expect(notifier).toBeInTheDocument();
+
+		act(() => jest.advanceTimersByTime(2000));
+		await waitFor(() =>
+			expect(
+				screen.queryByText(/Задача "купить хлеб" завершена/i),
+			).not.toBeInTheDocument(),
+		);
+	});
+
+	it('одновременно отображается только одно оповещение', async () => {
+		render(
+			<>
+				<NewTaskBar />
+				<TaskList />
+				<NotifierContainer />
+			</>,
+			{ wrapper: JestStoreProvider },
+		);
+
+		await addTaskUI('первая');
+		await addTaskUI('вторая');
+
+		await toggleTaskUI('первая');
+		const first = await screen.findByText(/"первая" завершена/i);
+		expect(first).toBeInTheDocument();
+
+		await toggleTaskUI('вторая');
+
+		const wrappers = screen.getAllByText(/Задача "/i);
+		expect(wrappers).toHaveLength(1);
+
+		expect(wrappers[0]).toHaveTextContent(/"вторая" завершена/i);
+	});
 });
